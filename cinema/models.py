a3537c523c1113065509610f35b2aa972d2f8717
Viesich <v.s.vesich@gmail.com>
2024-10-20 23:18:36 +0300

    create models and make migrations

--- cinema/models.py
+++ cinema/models.py
@@ -1,9 +1,35 @@
 from django.db import models
 
 
+class Genre(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return f"{self.last_name} {self.first_name}"
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=100)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.name} {self.rows} {self.seats_in_row}"
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
+    actors = models.ManyToManyField(Actor, related_name="movies")
+    genres = models.ManyToManyField(Genre, related_name="movies")
     duration = models.IntegerField()
 
     def __str__(self):
